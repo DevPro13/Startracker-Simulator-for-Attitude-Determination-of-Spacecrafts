228477393cb711a7e0112ad752a63420d3ef4176
Sanim Kumar Khatri <72692293+SanimKumarKhatri@users.noreply.github.com>
2023-08-02 08:01:49 +0545

    Added the smart triad sequence

--- pyramid algorithm/pyramid.py
+++ pyramid algorithm/pyramid.py
@@ -1,1 +1,11 @@
-print("Hello World")
+
+#smart sequence of triad indices
+n=5 #number of stars in the frame
+
+for dj in range(1, n-1):
+    for dk in range(1, n-dj):
+        for i in range(1, n-dk-dj+1):
+            j=i+dj
+            k=j+dk
+            print(f"[{i} {j} {k}]")
+
