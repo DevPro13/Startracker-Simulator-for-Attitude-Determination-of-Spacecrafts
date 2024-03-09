07c27b1f2178acb809fed84dca4d6a4132267faf
Sanim Kumar Khatri <72692293+SanimKumarKhatri@users.noreply.github.com>
2024-03-09 18:51:13 +0545

    Edited the quaternion calculation with radian

---
+++ startrack-demo/quarterneonCalc.py
@@ -0,0 +1,31 @@
+import math
+
+def radec_to_quarterneon(ra,dec,roll):
+    #initializing the quarterneon
+    q=[0,0,0,0]
+
+    #convering degrees to radians
+    ra = (ra * math.pi)/180
+    dec = (dec * math.pi)/180
+    roll = (roll * math.pi)/180
+
+    #calculating the direction cosines l,m,n of roll angle 
+    l = math.cos(dec)*math.cos(ra)
+    m = math.cos(dec)*math.sin(ra)
+    n = math.sin(dec)
+
+    #half-angle of roll
+    D = roll/2
+    vert_comp = math.sin(D)
+    horz_comp = math.cos(D)
+
+    #quarterneon components calculation
+    q[0]=l*vert_comp
+    q[1]=m*vert_comp
+    q[2]=n*vert_comp
+    q[3]=horz_comp
+    return q
+
+
+
+
